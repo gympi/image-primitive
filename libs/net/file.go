e32fb947fe63db8492bdf0926b5a1c9fc214e352
gympi <peter.arhipov@gmail.com>
2018-11-11 22:48:45 +0300

    refactoring

---
+++ libs/net/file.go
@@ -0,0 +1,59 @@
+package net
+
+import (
+  "net/http"
+  "io"
+  "os"
+  "path/filepath"
+)
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func GetFileContentType(out *os.File) (string, error) {
+
+    // Only the first 512 bytes are used to sniff the content type.
+    buffer := make([]byte, 512)
+
+    _, err := out.Read(buffer)
+    if err != nil {
+        return "", err
+    }
+
+    // Use the net/http package's handy DectectContentType function. Always returns a valid
+    // content-type by returning "application/octet-stream" if no others seemed to match.
+    contentType := http.DetectContentType(buffer)
+
+    return contentType, nil
+}
+
+func GetFileExt(filename string) string {
+
+    return filepath.Ext(filename)
+    // name := strings.TrimSuffix(basename, filepath.Ext(basename))
+    // return filename[len(extension):len(filename)]
+}
--- ui/handlers/api.go
+++ ui/handlers/api.go
@@ -5,10 +5,10 @@ import (
   "io"
   "log"
   "os"
-  "math/rand"
-  "time"
-  "path/filepath"
-  "os/exec"
+
+  "github.com/gympi/image-primitive/libs/string_utils"
+  "github.com/gympi/image-primitive/libs/net"
+  "github.com/gympi/image-primitive/libs/primitive_constructor"
 )
 
 func ApiPrimitive(w http.ResponseWriter, r *http.Request) {
@@ -28,13 +28,13 @@ func ApiPrimitive(w http.ResponseWriter, r *http.Request) {
 
     fileUrl := key
 
-    download_filepath := "public/static/shared/" + RandString(10) + GetFileExt(fileUrl)
-    err := DownloadFile(download_filepath, fileUrl)
+    download_filepath := "public/static/shared/" + string_utils.RandString(10) + net.GetFileExt(fileUrl)
+    err := net.DownloadFile(download_filepath, fileUrl)
     if err != nil {
         panic(err)
     }
 
-    filepath := build_primitive(download_filepath, )
+    filepath := primitive_constructor.PrimitiveConstructor(download_filepath, )
 
 
     img, err := os.Open(filepath)
@@ -58,110 +58,4 @@ func ApiPrimitive(w http.ResponseWriter, r *http.Request) {
           log.Println(err_r2)
           return
       }
-}
-
-
-// DownloadFile will download a url to a local file. It's efficient because it will
-// write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
-
-    // Create the file
-    out, err := os.Create(filepath)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
-
-    // Get the data
-    resp, err := http.Get(url)
-    if err != nil {
-        return err
-    }
-    defer resp.Body.Close()
-
-    // Write the body to file
-    _, err = io.Copy(out, resp.Body)
-    if err != nil {
-        return err
-    }
-
-    return nil
-}
-
-
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-var seededRand *rand.Rand = rand.New(
-  rand.NewSource(time.Now().UnixNano()))
-
-func StringWithCharset(length int, charset string) string {
-  b := make([]byte, length)
-  for i := range b {
-    b[i] = charset[seededRand.Intn(len(charset))]
-  }
-  return string(b)
-}
-
-func RandString(length int) string {
-  return StringWithCharset(length, charset)
-}
-
-func GetFileContentType(out *os.File) (string, error) {
-
-    // Only the first 512 bytes are used to sniff the content type.
-    buffer := make([]byte, 512)
-
-    _, err := out.Read(buffer)
-    if err != nil {
-        return "", err
-    }
-
-    // Use the net/http package's handy DectectContentType function. Always returns a valid
-    // content-type by returning "application/octet-stream" if no others seemed to match.
-    contentType := http.DetectContentType(buffer)
-
-    return contentType, nil
-}
-
-
-func GetFileExt(filename string) string {
-
-    return filepath.Ext(filename)
-    // name := strings.TrimSuffix(basename, filepath.Ext(basename))
-    // return filename[len(extension):len(filename)]
-}
-
-
-// primitive console params
-// https://github.com/fogleman/primitive
-type PrimitiveApiParams struct {
- i string //input file
- o string //output file
- n int //number of shapes
- m int //mode: 0=combo, 1=triangle, 2=rect, 3=ellipse, 4=circle, 5=rotatedrect, 6=beziers, 7=rotatedellipse, 8=polygon
-}
-
-func build_primitive(in string) string {
-    binary, lookErr := exec.LookPath("./vender/primitive")
-    if lookErr != nil {
-        log.Println(binary)
-        panic(lookErr)
-    }
-
-    primitive_filepath := "public/static/shared/primitive/" + RandString(10) + ".svg"
-    args := []string{"-i", in, "-o", primitive_filepath, "-n", "25"}
-
-    cmd := "./vender/primitive"
-
-    cmd_command := exec.Command(cmd, args...)
-    cmd_command.Start()
-
-    if err := cmd_command.Wait(); err != nil {
-        log.Println(err)
-    } else {
-        log.Println("Importing complete")
-    }
-
-    return primitive_filepath
 }
